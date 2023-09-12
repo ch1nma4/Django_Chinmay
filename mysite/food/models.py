e36a62fbbb1b0ee282e6e786862cb8a056d4b2b2
Chinmay <chinmaymayekar06@gmail.com>
2023-09-12 16:38:45 +0530

    commit:26

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -7,6 +7,10 @@ class Item(models.Model):
     Item_name = models.CharField(max_length=50)
     Item_desc = models.CharField(max_length=200)
     Item_price = models.IntegerField()
+    Item_image = models.CharField(
+        max_length=500 ,
+        default="https://w7.pngwing.com/pngs/277/489/png-transparent-fast-food-eating-maps-location-placeholder-pin-icon.png"
+    )
 
     def __str__(self):
         return self.Item_name 

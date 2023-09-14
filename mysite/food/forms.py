b4acae6fa13afffb2be001ce3cf5a4696958f52b
Chinmay <chinmaymayekar06@gmail.com>
2023-09-14 16:56:55 +0530

    commit:34

--- mysite/food/forms.py
+++ mysite/food/forms.py
@@ -4,5 +4,4 @@ from food.models import Item
 class ItemForm(forms.ModelForm):
     class Meta :
         model = Item
-        fields = ['prod_code' , 'for_user' , 'Item_name' , 'Item_desc' , 'Item_pric' , 'Item_image']
-        
+        fields = ['prod_code' , 'for_user' , 'Item_name' , 'Item_desc' , 'Item_price' , 'Item_image']

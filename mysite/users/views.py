7c835d2f31a377f6f48c960666d2573837eb0bc1
Chinmay <chinmaymayekar06@gmail.com>
2023-09-25 16:44:55 +0530

    commit:45

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from users.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth import authenticate , login
 
 # Create your views here.
 
@@ -27,3 +28,17 @@ def register(request):
     }
     
     return render(request,'users/register.html', context)
+
+def login_view(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request , username = username , password = password)
+
+        if user is not None:
+            login(request , user)
+            return redirect('food:index')
+        
+    return render (request, 'users/login.html')

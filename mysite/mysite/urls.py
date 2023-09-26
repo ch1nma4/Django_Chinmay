13993af9eff922935c48b6d911642a92845faa57
Chinmay <chinmaymayekar06@gmail.com>
2023-09-26 15:46:24 +0530

    commit:47

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('register/',users_views.register, name='register'),
     path('login/', users_views.login_view, name = 'login'),
     path('logout/',users_views.logout_view, name = 'logout'),
-
+    
 ]

95002ecd111bfd6c186ec86a6435eaff243d811e
Chinmay <chinmaymayekar06@gmail.com>
2023-10-17 16:51:40 +0530

    commit:73

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -137,6 +137,16 @@ def delete_item(request , id):
 
     if request.method == 'POST':
         item.delete()
+
+        Obj_History = History(
+            user_name = request.user.username,
+            prod_ref = item.prod_code,
+            item_name = item.Item_name,  #form.instance.item_name 
+            op_type =  'Deleted'
+        )
+
+        Obj_History.save()
+
         return redirect('food:index')
 
     return render(request , 'food/item-delete.html',context)

b3b9645699dd3fee6933299aa42313354926731a
Chinmay <chinmaymayekar06@gmail.com>
2023-10-19 16:53:22 +0530

    commit:79

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -7,6 +7,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from food.models import History
+from users.models import CusOrders
 
 # Create your views here.
 
@@ -50,10 +51,13 @@ def detail(request, item_id):
         prod_ref = item.prod_code
     )
 
+    Obj_CusOrd = CusOrders.objects.all()
+
     context = {
 
         'item' : item,
-        'hist' : hist
+        'hist' : hist,
+        'oco' : Obj_CusOrd
     }
     return render(request, 'food/detail.html', context)
 

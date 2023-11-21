a903c996ec3d6cb928688e5c069ecc4bbb75edd2
Chinmay <chinmaymayekar06@gmail.com>
2023-11-21 16:51:17 +0530

    commit:87

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -21,7 +21,9 @@ urlpatterns = [
 
     path('crf_upd/<int:details_id>/<int:crf_id>/', views.update_crf , name='upd_crf'),
 
-    
+    # deleting customer rating and feedback
+
+    path('del_crf/<int:details_id>/<int:crf_id>/', views.delete_crf , name='del_crf')
 
 
 ]
